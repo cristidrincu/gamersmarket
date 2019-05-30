122cf91449ae407cc78ba05cf746ad7e10a33268
Cristian Sergiu Drincu <cristidrincu@gmail.com>
2019-05-30 16:52:56 +0300

    Create initial persistence exception hierarchy and persistence exception mapper

---
+++ src/main/java/com/gamersmarket/common/utils/exceptions/PersistenceException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gamersmarket.common.utils.exceptions;
+
+/**
+ *
+ * @author cristiandrincu
+ */
+public class PersistenceException extends RuntimeException {
+    
+    public PersistenceException(String message) {
+        super(message);
+    }
+}
